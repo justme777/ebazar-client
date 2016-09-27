e5fea8931846d95ca5fcac95435e7a45a5a41e03
justme777 <ereke_enu@mail.ru>
2016-09-27 12:15:01 +0600

    knockout setting Load MainView

---
+++ utils/startup.js
@@ -0,0 +1,44 @@
+var ebazar = window.store||{};
+ebazar.vm =ebazar.vm||{};
+ebazar.util = ebazar.util||{}; 
+ebazar.util.startup = ebazar.util.startup || {};
+var model = ebazar.util.startup;
+
+model.loadConfig = function(){
+    $.getJSON("config.json", function (json) {
+        $.each(json, function (k, v) {
+            localStorage.setItem(k, v);
+        });
+    });
+}
+
+model.loadViews=function(){
+    //$("#sidebar-wrapper").load("/view/leftmenuview.html");
+    $("#mainview").load("/view/mainview.html");    
+}
+
+model.login = function(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var postData = { 'username': username, 'password': password };
+
+
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: localStorage.getItem("services_url") + "system/auth?token=0",
+        data: JSON.stringify(postData),
+        dataType: "json",
+        success: function (res) {
+            alert(res);
+            localStorage.setItem("token", res);
+            if (res == 401) {
+
+            }
+            else {
+                window.location = "index.html";
+            }
+        }
+    });
+}
