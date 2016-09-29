e0b7ff51d072901d2b901294b927ff4bc9fdc8ac
justme777 <ereke_enu@mail.ru>
2016-09-29 23:54:54 +0600

    create point view .html

--- viewmodel/adminmenu.js
+++ viewmodel/adminmenu.js
@@ -2,5 +2,9 @@ ebazar.vm.adminmenu = ebazar.vm.adminmenu||{};
 var model = ebazar.vm.adminmenu;
 
 model.showAddMarketView = function(){
-    $("#mainview").load("/view/admin/create_market_view.html");
+    $("#mainview").load("../view/admin/create_market_view.html");
+}
+
+model.showAddPointView = function(){
+    $("#mainview").load("../view/admin/create_point_view.html");
 }
